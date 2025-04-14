f1980e306001809f9318982b97b54ee9cd7139a3
Manoj Mahato <127175133+Manoj-Mahat0@users.noreply.github.com>
2025-04-14 18:47:25 +0530

    Update database.py
    db update

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./datam.db"
+DATABASE_URL = "mysql+pymysql://sql12772737:IHYzNkIyUY@sql12.freesqldatabase.com:3306/sql12772737"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
